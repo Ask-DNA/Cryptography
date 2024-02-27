719f6a8516036f06d52ca724f14584a9573b917a
Ask-DNA <alexander.sukhanov.11@gmail.com>
2024-02-27 11:47:15 +0300

    RSA update. Enable prime multipliers byte length settings. Add data size check before encoding.

--- Cryptography/Encoders/Asymmetric/RSA.cs
+++ Cryptography/Encoders/Asymmetric/RSA.cs
@@ -2,9 +2,19 @@
 
 namespace Cryptography.Encoders.Asymmetric
 {
-    public class RSA(int? seed = null)
+    public class RSA
     {
-        private readonly Random _random = (seed == null) ? new() : new(seed.Value);
+        private readonly Random _random;
+        private readonly BigInteger _minP, _minQ;
+
+        public RSA(int pMinByteLen = 32, int qMinByteLen = 32, int? seed = null)
+        {
+            ArgumentOutOfRangeException.ThrowIfLessThan(pMinByteLen, 2);
+            ArgumentOutOfRangeException.ThrowIfLessThan(qMinByteLen, 2);
+            _random = (seed == null) ? new() : new(seed.Value);
+            _minP = BigIntegerExtensions.MinPositive(pMinByteLen);
+            _minQ = BigIntegerExtensions.MinPositive(qMinByteLen);
+        }
 
         public void GenerateKeys(out (byte[] e, byte[] n) publicKey, out (byte[] d, byte[] n) privateKey)
         {
@@ -15,27 +25,54 @@ namespace Cryptography.Encoders.Asymmetric
 
         public void GenerateKeys(out (BigInteger e, BigInteger n) publicKey, out (BigInteger d, BigInteger n) privateKey)
         {
-            BigInteger min = BigIntegerExtensions.MinPositive(32);
-
-            BigInteger p = BigIntegerExtensions.ProbablePrime(min, _random);
-            BigInteger q = BigIntegerExtensions.ProbablePrime(min, _random);
-            BigInteger n = p * q;
+            BigInteger p = BigIntegerExtensions.ProbablePrime(_minP, _random);
+            BigInteger q = BigIntegerExtensions.ProbablePrime(_minQ, _random);
+            BigInteger module = p * q;
             BigInteger euler = (p - 1) * (q - 1);
-            BigInteger e = 65537;
-            BigInteger d = e.ModularMultiplicativeInverse(euler);
-            publicKey = (e, n);
-            privateKey = (d, n);
+            BigInteger publicExponent = 65537;
+            BigInteger privateExponent = publicExponent.ModularMultiplicativeInverse(euler);
+            publicKey = (publicExponent, module);
+            privateKey = (privateExponent, module);
         }
 
-        public static byte[] Sifer(byte[] data, (byte[] exp, byte[] n) key)
+        public static byte[] Sifer(byte[] data, (byte[] exponent, byte[] module) key)
         {
-            (BigInteger exp, BigInteger n) = (new(key.exp), new(key.n));
-            return BigInteger.ModPow(new(data), exp, n).ToByteArray();
+            (BigInteger exponentInt, BigInteger moduleInt) = (new(key.exponent), new(key.module));
+            BigInteger dataInt = new(data);
+            if (dataInt >= moduleInt)
+                throw new ArgumentException($"Integer representation of '{nameof(data)}' must be less than '{nameof(key.module)}'.");
+            return BigInteger.ModPow(dataInt, exponentInt, moduleInt).ToByteArray();
         }
 
-        public static BigInteger Sifer(BigInteger data, (BigInteger exp, BigInteger n) key)
+        public static BigInteger Sifer(BigInteger data, (BigInteger exponent, BigInteger module) key)
         {
-            return BigInteger.ModPow(data, key.exp, key.n);
+            if (data >= key.module)
+                throw new ArgumentException($"'{nameof(data)}' must be less than '{nameof(key.module)}'.");
+            return BigInteger.ModPow(data, key.exponent, key.module);
+        }
+
+        public static bool TrySifer(byte[] data, (byte[] exponent, byte[] module) key, out byte[] result)
+        {
+            (BigInteger exponentInt, BigInteger moduleInt) = (new(key.exponent), new(key.module));
+            BigInteger dataInt = new(data);
+            if (dataInt >= moduleInt)
+            {
+                result = [];
+                return false;
+            }
+            result = BigInteger.ModPow(dataInt, exponentInt, moduleInt).ToByteArray();
+            return true;
+        }
+
+        public static bool TrySifer(BigInteger data, (BigInteger exponent, BigInteger module) key, out BigInteger result)
+        {
+            if (data >= key.module)
+            {
+                result = 0;
+                return false;
+            }
+            result = BigInteger.ModPow(data, key.exponent, key.module);
+            return true;
         }
     }
 }
